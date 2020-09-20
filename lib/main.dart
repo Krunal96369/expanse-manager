201d4d980baf3acd2917354ed19374a366e79f35
Krunal Chauhan <59173615+Whokrunalchauhan@users.noreply.github.com>
2020-09-20 11:39:09 +0530

    change #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+//import 'package:flutter/services.dart';
 
 import 'models/transaction.dart';
 import 'widgets/new_transaction.dart';
@@ -105,22 +106,36 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
-    final appbar = AppBar(
-      actions: [
-        IconButton(
-            icon: Icon(
-              Icons.add,
-              color: Theme.of(context).primaryColor,
-              size: 25,
-              semanticLabel: 'add',
+    final PreferredSizeWidget appbar = Platform.isAndroid
+        ? AppBar(
+            actions: [
+              IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Theme.of(context).primaryColor,
+                    size: 25,
+                    semanticLabel: 'add',
+                  ),
+                  onPressed: () => _showModalsheet(context))
+            ],
+            title: Text(
+              'Expanse manager',
+              style: TextStyle(color: Theme.of(context).primaryColor),
             ),
-            onPressed: () => _showModalsheet(context))
-      ],
-      title: Text(
-        'Expanse manager',
-        style: TextStyle(color: Theme.of(context).primaryColor),
-      ),
-    );
+          )
+        : CupertinoNavigationBar(
+            middle: Text('Expanse manager',
+                style: TextStyle(color: Theme.of(context).primaryColor)),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  child: Icon(CupertinoIcons.add),
+                  onTap: () => _showModalsheet(context),
+                )
+              ],
+            ),
+          );
     final transactionlistWidget = Container(
       height: (MediaQuery.of(context).size.height -
               appbar.preferredSize.height -
@@ -128,56 +143,69 @@ class _MyHomePageState extends State<MyHomePage> {
           0.7,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
-
-    return Scaffold(
-      appBar: appbar,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            if (isLandscape)
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Show Chart'),
-                  Switch.adaptive(
-                    activeColor: Theme.of(context).accentColor,
-                    value: _showChart,
-                    onChanged: (val) {
-                      setState(() {
-                        _showChart = val;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            if (!isLandscape)
-              Container(
-                height: (MediaQuery.of(context).size.height -
-                        appbar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
-                    0.3,
-                child: Chart(_recentTransactions),
-              ),
-           if(!isLandscape) transactionlistWidget,
-            if (isLandscape)
-              _showChart
-                  ? Container(
-                      height: (MediaQuery.of(context).size.height -
-                              appbar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
-                          0.7,
-                      child: Chart(_recentTransactions),
-                    )
-                  : transactionlistWidget,
-          ],
-        ),
+    final bodyContent = SafeArea(
+        child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          if (isLandscape)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Show Chart',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Switch.adaptive(
+                  activeColor: Theme.of(context).accentColor,
+                  value: _showChart,
+                  onChanged: (val) {
+                    setState(() {
+                      _showChart = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+          if (!isLandscape)
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appbar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.3,
+              child: Chart(_recentTransactions),
+            ),
+          if (!isLandscape) transactionlistWidget,
+          if (isLandscape)
+            _showChart
+                ? Container(
+                    height: (MediaQuery.of(context).size.height -
+                            appbar.preferredSize.height -
+                            MediaQuery.of(context).padding.top) *
+                        0.7,
+                    child: Chart(_recentTransactions),
+                  )
+                : transactionlistWidget,
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Platform.isAndroid? FloatingActionButton(
-        onPressed: () => _showModalsheet(context),
-        child: Icon(Icons.add),
-      ):Container(),
-    );
+    ),);
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: bodyContent,
+            navigationBar: appbar,
+          )
+        : Scaffold(
+            appBar: appbar,
+            body: bodyContent,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isAndroid
+                ? FloatingActionButton(
+                    onPressed: () => _showModalsheet(context),
+                    child: Icon(Icons.add),
+                  )
+                : Container(),
+          );
   }
 }
